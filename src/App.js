45075f8d5221b4bfb0b905ad9bb461bde0267f3e
haikalmsg <117971970+haikalmsg@users.noreply.github.com>
2023-11-15 11:48:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    password : ""
+  })
+  function handleChange(e){
+    const {name, value} = e.target
+    setFormData(olddata => {
+      return{
+        ...olddata,
+        [name] : value
+      }
+    })
+  }
+  function check(pass){
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/; 
+    return regex.test(pass)
+  }
+  let flag = false
+  let pw = false;
+  if (formData.password.length == 0){
+    flag = false
+  }
+  else{
+    flag = true
+    pw = check(formData.password)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='passwordcheck'>
+        <input className='passwordEnter' placeholder='password' name = "password" value={formData.password} onChange={handleChange} ></input>
+        {flag && <h3 className='describe'>{pw ? " ✅ Strong Password" : "❌ Weak Password"}</h3>}
+
+      </form>
     </div>
   );
 }
